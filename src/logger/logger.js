e4d1557ff992c58c5910d47c25490cc5d5ad184a
artirajpoot377 <artirajpoot@377gmail.com>
2022-10-16 12:53:09 +0530

    modification

---
+++ src/logger/logger.js
@@ -0,0 +1,16 @@
+const url = "https://www.google.com"
+
+/**let printSomething = function() {
+    console.log("Request details are - a, b, c")
+    return "done"
+}**/
+
+let printwelcome= function(){
+    return("welcome .my name is arti rajpoot") 
+}
+
+
+let abc = 2022
+//module.exports.myUrl = url
+//odule.exports.myFunction = printSomething
+module.exports.myfunction1=printwelcome

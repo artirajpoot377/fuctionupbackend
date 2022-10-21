4e501d7af086f659fd22481ec9693aaaba226317
artirajpoot377 <artirajpoot@377gmail.com>
2022-10-21 23:47:09 +0530

    modify

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://artirajpoot:Btech2023@cluster0.korxmrm.mongodb.net/artirajpoot377?retryWrites=true&w=majority"
 , {
    useNewUrlParser: true 
 }
